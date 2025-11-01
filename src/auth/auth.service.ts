2f19e4927c8609c36d8ccf94ab867d710cc064c1
Daniel <jamoja64749@gmail.com>
2025-11-01 17:24:12 +0100

    Implemented JWT token signing and user validation logic using JwtService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
+// TODO: make password encrypted and later compare the hashed password not the plain text password
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private userService: UserService,
+        private jwtService: JwtService
+    ) {}
+
+    async signIn(
+        username: string,
+         pass: string
+    ): Promise<{ access_token: string }> {
+        const user = await this.userService.findOne(username);
+        if(user?.password !== pass){
+            throw new UnauthorizedException();
+        }   
+        const payload = { sub: user.userId, username: user.username };
+
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+        };
+    }
+}
