44ce1118c92eebbda71dd9f896ca787cf7cae9da
Daniel <jamoja64749@gmail.com>
2025-11-01 17:24:39 +0100

    Added login and profile routes with JWT authentication flow

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Get, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 
-@Controller('auth')
-export class AuthController {}
+// TODO: Change the Record<string, any> to use a Dto to validate the data thats getting recieved
+
+@Controller('/auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @HttpCode(HttpStatus.OK)
+    @Post('/login')
+    signIn(@Body() signInDto: Record<string, any>){
+        return this.authService.signIn(signInDto.username, signInDto.password);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('/profile')
+    getProfile(@Request() req) {
+        return req.user;
+    }
+
+}
