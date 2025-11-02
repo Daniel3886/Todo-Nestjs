50bafed76e641e431ccb0935668439c734a2c6ae
Daniel <jamoja64749@gmail.com>
2025-11-02 20:11:30 +0100

    Added a /refresh endpoint to issue new access tokens using a refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Get, UseGuards, Request } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Get, UseGuards, Request, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 import { SignInDto } from './dto/sign-in.dto';
-
-// TODO: Change the Record<string, any> to use a Dto to validate the data thats getting recieved
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('/auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+        
+    constructor(
+        private authService: AuthService,
+        private jwtService: JwtService
+    ) {}
 
     @HttpCode(HttpStatus.OK)
     @Post('/login')
@@ -21,4 +24,14 @@ export class AuthController {
         return req.user;
     }
 
+    @Post('refresh')
+    async refresh(@Body('refreshToken') token: string) {
+      try {
+        const payload = this.jwtService.verify(token);
+        const newAccessToken = this.jwtService.sign({ sub: payload.sub });
+        return { accessToken: newAccessToken };
+      } catch (e) {
+        throw new UnauthorizedException('Invalid refresh token');
+    }
+}
 }
