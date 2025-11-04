8c94a67e733c4e4b2f6bae94f366787c9fdec49f
Daniel <jamoja64749@gmail.com>
2025-11-04 22:59:54 +0100

    Implemented register logic with bcrypt password hashing and token generation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, Get, UseGuards, Request, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
-import { SignInDto } from './dto/sign-in.dto';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 import { JwtService } from '@nestjs/jwt';
 
 @Controller('/auth')
@@ -13,25 +14,32 @@ export class AuthController {
     ) {}
 
     @HttpCode(HttpStatus.OK)
-    @Post('/login')
-    signIn(@Body() signInDto: SignInDto){
-        return this.authService.signIn(signInDto.email, signInDto.password);
+    @Post('/register')
+    register(@Body() registerDto: RegisterDto){
+        return this.authService.register(registerDto.email, registerDto.password);
+            
     }
 
+    @HttpCode(HttpStatus.OK)
+    @Post('/login')
+    login(@Body() loginDto: LoginDto){
+        return this.authService.login(loginDto.email, loginDto.password);
+    }
+
+    @Post('/refresh')
+    async refresh(@Body('refreshToken') token: string) {
+        try {
+            const payload = this.jwtService.verify(token);
+            const newAccessToken = this.jwtService.sign({ sub: payload.sub });
+            return { accessToken: newAccessToken };
+        } catch (e) {
+            throw new UnauthorizedException('Invalid refresh token');
+        }
+    }
+    
     @UseGuards(AuthGuard)
     @Get('/profile')
     getProfile(@Request() req) {
         return req.user;
     }
-
-    @Post('refresh')
-    async refresh(@Body('refreshToken') token: string) {
-      try {
-        const payload = this.jwtService.verify(token);
-        const newAccessToken = this.jwtService.sign({ sub: payload.sub });
-        return { accessToken: newAccessToken };
-      } catch (e) {
-        throw new UnauthorizedException('Invalid refresh token');
-    }
-}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,7 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, BadRequestException } from '@nestjs/common';
 import { UserService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
-
-// TODO: make password encrypted and later compare the hashed password not the plain text password
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,17 +10,41 @@ export class AuthService {
         private jwtService: JwtService
     ) {}
 
-    async signIn(
+    async register(
         email: string, 
-        pass: string
+        password: string
     ): Promise<{ access_token: string, refresh_token: string }> {
+        const exist = await this.userService.findOne(email);
+
+        if(exist){
+            throw new BadRequestException("Email already registered");
+        }   
+
+        const user = await this.userService.create(email, password);
+        const payload = { sub: user.id, email: user.email }
+
+        return {
+            access_token: this.jwtService.sign(payload),
+            refresh_token: this.jwtService.sign(payload, { expiresIn: '7d'})
+        };
+    }
+
+    async login(
+        email: string, 
+        password: string
+    ): Promise<{ access_token: string; refresh_token: string }> {
         const user = await this.userService.findOne(email);
 
-        if(user?.password !== pass){
-            throw new UnauthorizedException();
+        if(!user){
+            throw new UnauthorizedException("Invalid email");
         }   
+
+        const passwordMatches = await bcrypt.compare(password, user.password);
+        if(!passwordMatches) {
+            throw new UnauthorizedException("Invalid password");
+        }
         
-        const payload = { sub: user.userId, email: user.email };
+        const payload = { sub: user.id, email: user.email };
 
         return {
             access_token: this.jwtService.sign(payload),
