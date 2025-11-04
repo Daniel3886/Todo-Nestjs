c2f9f7942b6fd4f2d11ddbf6a76abd4adcd462cb
Daniel <jamoja64749@gmail.com>
2025-11-04 23:01:41 +0100

    Updated UserService for Prisma integration and password hashing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,19 @@
 import { Injectable } from '@nestjs/common';
-export type User = any; 
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-    private readonly users = [
-        // FIXME: use a database instead of hardcoding the values in users
-        {
-            userId: 1,
-            email: "john@gmail.com",
-            password: "password"
-        },
-        {
-            userId: "2",
-            email: "maria@proton.me",
-            password: "guess"
-        }
-    ];
+  constructor(private prisma: PrismaService) {}
 
-    async findOne(email: string): Promise<User | undefined> {
-        return this.users.find(user => user.email === email)
-    }
+  async create(email: string, password: string) {
+    const hashed = await bcrypt.hash(password, 10);
+    return this.prisma.user.create({
+      data: { email, password: hashed },
+    });
+  }
+
+  async findOne(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
 }
