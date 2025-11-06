c32e06eb9da4fe11bd10fff3b073076da0fa3aef
Daniel <jamoja64749@gmail.com>
2025-11-06 20:29:23 +0100

    Implemented refresh token handling and logout logic in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,7 +13,7 @@ export class AuthService {
     async register(
         email: string, 
         password: string
-    ): Promise<{ access_token: string, refresh_token: string }> {
+    ): Promise<{ accessToken: string, refreshToken: string }> {
         const exist = await this.userService.findOne(email);
 
         if(exist){
@@ -22,17 +22,19 @@ export class AuthService {
 
         const user = await this.userService.create(email, password);
         const payload = { sub: user.id, email: user.email }
+        const accessToken = this.jwtService.sign(payload);
+        const refreshToken = this.jwtService.sign(payload, { expiresIn: '7d'});
 
-        return {
-            access_token: this.jwtService.sign(payload),
-            refresh_token: this.jwtService.sign(payload, { expiresIn: '7d'})
-        };
+        const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+        await this.userService.updateRefreshToken(user.id, hashedRefreshToken);
+
+        return { accessToken, refreshToken};
     }
 
     async login(
         email: string, 
         password: string
-    ): Promise<{ access_token: string; refresh_token: string }> {
+    ): Promise<{ accessToken: string; refreshToken: string }> {
         const user = await this.userService.findOne(email);
 
         if(!user){
@@ -45,10 +47,33 @@ export class AuthService {
         }
         
         const payload = { sub: user.id, email: user.email };
+        const accessToken = this.jwtService.sign(payload);
+        const refreshToken = this.jwtService.sign(payload, { expiresIn: '7d'});
 
-        return {
-            access_token: this.jwtService.sign(payload),
-            refresh_token: this.jwtService.sign(payload, { expiresIn: '7d'})
-        };
+        const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+        await this.userService.updateRefreshToken(user.id, hashedRefreshToken);
+
+        return { accessToken, refreshToken };
+    };
+
+    async refresh(token: string): Promise<{ accessToken: string }> {
+        try {
+        const payload = this.jwtService.verify(token);
+        const user = await this.userService.findById(payload.sub);
+
+        if (!user || !user.refreshToken) throw new UnauthorizedException('Invalid refresh token');
+
+        const isMatch = await bcrypt.compare(token, user.refreshToken);
+        if (!isMatch) throw new UnauthorizedException('Invalid refresh token');
+
+        const newAccessToken = this.jwtService.sign({ sub: user.id, email: user.email });
+        return { accessToken: newAccessToken };
+    } catch (e) {
+        throw new UnauthorizedException('Invalid or expired refresh token');
+        }
+    }
+
+    async logout(userId: number){
+        await this.userService.removeRefreshToken(userId);
     }
 }
