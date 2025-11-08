c9ad6862fe7ddb9673bf3c3e7c95e7651aa3a3cd
Daniel <jamoja64749@gmail.com>
2025-11-08 21:56:14 +0100

    Initialized prisma with a todo model

---
+++ prisma/migrations/20251108182128_add_todo_model/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('DONE', 'PENDING');
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" "Status" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "dueDate" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
