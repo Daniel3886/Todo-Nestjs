011cd68509352add79fdf0194e6d4c9e64b10f3b
Daniel <jamoja64749@gmail.com>
2025-11-09 19:49:58 +0100

    Added Swagger setup with API title, description, version, and JWT auth

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo App API')
+    .setDescription(
+      `An **online task management service**, where each user has their **own personal account**.
+      After logging in, the user can **create, edit, and delete** their own tasks.
+      Each task may optionally have a **due date**.`,
+    )
+    .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+      description: 'Enter JWT token: ',
+      in: 'header',
+    },
+    'access-token',
+  )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
