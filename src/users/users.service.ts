f6d6c1168e30b7a2b651fdf460d34a8646954c45
Daniel <jamoja64749@gmail.com>
2025-11-14 22:38:41 +0100

    fix(users): update id and improve type safety of all methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,37 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { Session, User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(email: string, password: string) {
+  async create({ email, password }: { email: string; password: string }) {
     const hashed = await bcrypt.hash(password, 10);
     return this.prisma.user.create({
       data: { email, password: hashed },
     });
   }
 
-  async findOne(email: string) {
+  async findOne(email: User['email']) {
     return this.prisma.user.findUnique({ where: { email } });
   }
 
-  async updateRefreshToken(user_id: number, refreshToken: string) {
-    return this.prisma.user.update({
-      where: { id: user_id },
-      data: { refreshToken }
+  async createSession(
+    userId: User['id'],
+    refreshToken: Session['refreshTokens'],
+  ) {
+    return this.prisma.session.create({
+      data: {
+        userId,
+        refreshTokens: refreshToken,
+      },
     });
   }
 
-  async removeRefreshToken(user_id: number) {
-    return this.prisma.user.update({
-      where: {id: user_id},
-      data: { refreshToken: null },
-    })
+  async findSessionByToken(refreshToken: Session['refreshTokens']) {
+    return this.prisma.session.findFirst({
+      where: { refreshTokens: refreshToken },
+    });
   }
 
-  async findById(id: number){
-    return this.prisma.user.findUnique({ where: { id }})
+  async removeAllSessionsForUser(userId: User['id']) {
+    return this.prisma.session.deleteMany({
+      where: { userId },
+    });
+  }
+
+  async findById(id: User['id']) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async comparePassword(
+    inputPassword: string,
+    hashedPassword: User['password'],
+  ): Promise<boolean> {
+    return bcrypt.compare(inputPassword, hashedPassword);
   }
 }
