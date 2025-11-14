601106b73f7d97908c5b9250a0c52c568da45fc9
Daniel <jamoja64749@gmail.com>
2025-11-14 22:18:32 +0100

    fix(todo): use only id field in Prisma unique operations

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -3,103 +3,99 @@ import { CreateTodoDto } from './dto/create-todo.dto';
 import { FilterTodoDto } from './dto/filter-todo.dto';
 import { EditTodoDto } from './dto/edit-todo.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { connect } from 'http2';
-import { constants } from 'buffer';
-import { contains } from 'class-validator';
-import { UsersModule } from 'src/users/users.module';
+import { Prisma, User } from '@prisma/client';
 
 @Injectable()
 export class TodoService {
-    constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  async createTodo(userId: number, dto: CreateTodoDto) {
+  async createTodo({
+    userId,
+    dto,
+  }: {
+    userId: User['id'];
+    dto: CreateTodoDto;
+  }) {
     return this.prisma.todo.create({
-        data: {
-            title: dto.title,
-            description: dto.description,
-            dueDate: dto.dueDate ? new Date(dto.dueDate) : null,
-            user: {
-                connect: { id: userId },
-            }
-        }
-    })
+      data: {
+        ...dto,
+        userId,
+      },
+    });
   }
 
-  async getTodo(userId: number, dto: FilterTodoDto) {
-  const where: any = { userId };
-
-  if (dto.status) where.status = dto.status;
-  if (dto.query) {
-    where.OR = [
-      { title: { contains: dto.query, mode: 'insensitive' } },
-      { description: { contains: dto.query, mode: 'insensitive' } },
-    ];
-  }
-  if (dto.before || dto.after) {
-    where.dueDate = {};
-    if (dto.after) where.dueDate.gte = new Date(dto.after);
-    if (dto.before) where.dueDate.lte = new Date(dto.before);
-  }
-  if (dto.includeNoDueDate) {
-    where.OR = [...(where.OR || []), { dueDate: null }];
-  }
-
-  return this.prisma.todo.findMany({
-    where,
-    take: dto.limit ?? 10,
-    skip: dto.page ? (dto.page - 1) * (dto.limit ?? 10) : 0,
-  });
-}
-
-  async getTodoById(userId: number, id: number) {
-    const todo = await this.prisma.todo.findUnique({
-        where: {
-            id: id,
-            userId: userId,
+  async getTodo({ userId, dto }: { userId: string; dto: FilterTodoDto }) {
+    const { query, status, startDate, endDate, includeNoDueDate } = dto;
+    const where: Prisma.TodoWhereInput = {
+      AND: [
+        {
+          userId,
+          status,
         },
-    })
+        {
+          OR: query
+            ? [
+                { title: { contains: query, mode: 'insensitive' } },
+                { description: { contains: query, mode: 'insensitive' } },
+              ]
+            : undefined,
+        },
+        {
+          dueDate:
+            startDate || endDate ? { gte: startDate, lte: endDate } : undefined,
+        },
+        {
+          OR: includeNoDueDate ? [{ dueDate: null }] : undefined,
+        },
+      ],
+    } satisfies Prisma.TodoWhereInput;
 
-    if(!todo || todo.userId !== userId) {
-        throw new NotFoundException(`Todo with id ${id} not found`);
+    return this.prisma.todo.findMany({
+      where,
+      take: dto.limit,
+      skip: dto.page ? (dto.page - 1) * (dto.limit ?? 10) : 0,
+    });
+  }
+
+  async getTodoById({ userId, id }: { userId: string; id: string }) {
+    const todo = await this.prisma.todo.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!todo || todo.userId !== userId) {
+      throw new NotFoundException(`Todo with id ${id} not found`);
     }
 
     return todo;
   }
 
-  async editTodo(userId: number, id: number, dto: EditTodoDto) {
-    try{
+  async editTodo(
+    { userId, id }: { userId: User['id']; id: string },
+    dto: EditTodoDto,
+  ) {
+    await this.getTodoById({ userId, id });
+
     const updatedTodo = await this.prisma.todo.update({
-        where: {
-            userId: userId,
-            id: id
-        },
-        data: {
-            title: dto.title,
-            description: dto.description,
-            status: dto.status as 'DONE' | 'PENDING',
-            dueDate: dto.dueDate ? new Date(dto.dueDate) : null,
-        }
-    })
+      where: {
+        id: id,
+      },
+      data: dto,
+    });
 
     return updatedTodo;
-    } catch (e){
-        throw new NotFoundException(`Todo with id ${id} not found`);
-    }
   }
 
-  async deleteTodo(userId: number, id: number) {
-    try{
-    const deleteTodo = await this.prisma.todo.delete({
-        where: {
-            id,
-            userId
-        }
-    })
+  async deleteTodo({ userId, id }: { userId: User['id']; id: string }) {
+    await this.getTodoById({ userId, id });
 
-    return deleteTodo;
-    } catch (e){
-        throw new NotFoundException(`Todo with id ${id} not found`);
-    }
+    const deletedTodo = await this.prisma.todo.delete({
+      where: {
+        id,
+      },
+    });
+
+    return deletedTodo;
   }
 }
-
