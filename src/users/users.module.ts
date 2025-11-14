308961a9c23091c239227957271ba867a6a2e228
Daniel <jamoja64749@gmail.com>
2025-11-14 22:37:46 +0100

    chore(users): import PrismaModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './users.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [UserService],
   exports: [UserService],
 })
