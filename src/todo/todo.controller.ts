cc41f2d9e32d77934c788f76691a5a4f818f89d3
Daniel <jamoja64749@gmail.com>
2025-11-14 22:36:59 +0100

    fix(todo): ensure type safety across the endpoints

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,41 +1,57 @@
-import { Controller, Get, Post, Patch, Delete, Body, Param, Query, UseGuards, Request, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { EditTodoDto } from './dto/edit-todo.dto';
 import { FilterTodoDto } from './dto/filter-todo.dto';
 import { TodoService } from './todo.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AuthUser } from 'src/auth/decorators/auth.decorator';
+import type { JwtPayload } from 'src/auth/types/jwt-payload.type';
+import { SwaggerConfig } from 'src/config/swagger.config';
 
-@ApiBearerAuth('access-token')
-@ApiTags('todo')
+@ApiBearerAuth(SwaggerConfig.todo.bearerAuthName)
+@ApiTags(SwaggerConfig.todo.tag)
 @UseGuards(AuthGuard)
-@Controller('/todo')
+@Controller('/todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @Post()
-  async createTodo(@Body() dto: CreateTodoDto, @Request() req) {
-    console.log(req.user);
-    return this.todoService.createTodo(req.user.id, dto);
+  async createTodo(@Body() dto: CreateTodoDto, @AuthUser() user: JwtPayload) {
+    return this.todoService.createTodo({ userId: user.id, dto });
   }
 
   @Get()
-  async getTodo(@Query() dto: FilterTodoDto, @Request() req) {
-    return this.todoService.getTodo(req.user.id, dto);
+  async getTodo(@Query() dto: FilterTodoDto, @AuthUser() user: JwtPayload) {
+    return this.todoService.getTodo({ userId: user.id, dto });
   }
 
-  @Get('/:id')
-  async getTodokById(@Param('id', ParseIntPipe) id: number, @Request() req) {
-    return this.todoService.getTodoById(req.user.id, id);
+  @Get(':id')
+  async getTodoById(@Param('id') id: string, @AuthUser() user: JwtPayload) {
+    return this.todoService.getTodoById({ userId: user.id, id: id });
   }
 
-  @Patch('/:id')
-  async editTodo(@Param('id', ParseIntPipe) id: number, @Body() dto: EditTodoDto, @Request() req) {
-    return this.todoService.editTodo(req.user.id, id, dto);
+  @Patch(':id')
+  async editTodo(
+    @Param('id') id: string,
+    @Body() dto: EditTodoDto,
+    @AuthUser() user: JwtPayload,
+  ) {
+    return this.todoService.editTodo({ userId: user.id, id: id }, dto);
   }
 
-  @Delete('/:id')
-  async deleteTodo(@Param('id', ParseIntPipe) id: number, @Request() req) {
-    return this.todoService.deleteTodo(req.user.id, id);
+  @Delete(':id')
+  async deleteTodo(@Param('id') id: string, @AuthUser() user: JwtPayload) {
+    return this.todoService.deleteTodo({ userId: user.id, id: id });
   }
 }
