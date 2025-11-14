8296ed6be36d167558c145d7c6fda4e39a1900f7
Daniel <jamoja64749@gmail.com>
2025-11-14 22:36:05 +0100

    fix(todo): refactor FilterTodoDto to handle query, status, pagination with type safety

--- src/todo/dto/filter-todo.dto.ts
+++ src/todo/dto/filter-todo.dto.ts
@@ -1,36 +1,49 @@
-import { IsOptional, IsIn, IsDateString, IsBoolean, IsInt, Min, IsString,  } from "class-validator";
-import { Type } from "class-transformer";
+import {
+  IsOptional,
+  IsBoolean,
+  IsInt,
+  Min,
+  IsString,
+  IsEnum,
+  MaxLength,
+  IsDate,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { $Enums } from '@prisma/client';
 
 export class FilterTodoDto {
   @IsOptional()
-  status?: 'DONE' | 'PENDING';
+  @IsEnum($Enums.Status)
+  status?: $Enums.Status;
 
   @IsOptional()
   @IsString()
+  @MaxLength(255)
   query?: string;
 
   @IsOptional()
-  @IsDateString()
-  before?: string;
+  @IsDate()
+  @Type(() => Date)
+  startDate?: Date;
 
   @IsOptional()
-  @IsDateString()
-  after?: string;
+  @IsDate()
+  @Type(() => Date)
+  endDate?: Date;
 
   @IsOptional()
   @IsBoolean()
   @Type(() => Boolean)
   includeNoDueDate?: boolean;
 
-  @IsOptional()
   @IsInt()
   @Min(1)
   @Type(() => Number)
-  page?: number = 1;
+  page: number = 1;
 
   @IsOptional()
   @IsInt()
   @Min(1)
   @Type(() => Number)
-  limit?: number = 10;
+  limit: number = 10;
 }
